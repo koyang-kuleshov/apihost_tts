f465df038496ffdb7252db7f160c4a04f8fa82ff
Сол Коган <45554270+solkogan@users.noreply.github.com>
2020-04-23 14:01:34 +0600

    Add files via upload

--- apihostTTS.py
+++ apihostTTS.py
@@ -18,21 +18,21 @@ def playmp3(filepath):
 # Функция говорящая фразу вслух
 def speakme(mytext):
     print('Робот сказал: '+mytext)
-    mytext=mytext.replace(' ','+')
-    myurl='https://apihost.ru/php/voice.php?&text='+mytext+'&format=mp3&lang=ru-RU&speed=1.0&emotion=neutral&speaker=oksana&robot=1&download'
-    r = requests.get(myurl, stream=True)
-    if r.status_code == 200:
-        fm='mp3/'+re.sub('[^а-яА-Я]', '', mytext)
-        fm=fm[:200]
-        fm=fm+'.mp3'
-        if not(os.path.exists(fm)):
+    fm='mp3/'+re.sub('[^а-яА-Я]', '', mytext)
+    fm=fm[:200]
+    fm=fm+'.mp3'
+    if not(os.path.exists(fm)): 
+        mytext=mytext.replace(' ','+')
+        myurl='https://apihost.ru/php/voice.php?&text='+mytext+'&format=mp3&lang=ru-RU&speed=1.0&emotion=neutral&speaker=oksana&robot=1&download'
+        r = requests.get(myurl, stream=True)
+        if r.status_code == 200:
             with open(fm, 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
             f.close()
-        playmp3(fm)
+    playmp3(fm)
 
-speakme('Приветствую тебя, человек!')
+speakme('Приветствую тебя, Иван!')
 speakme('Сегодня мы научились использовать синтез речи на радость людям!')
 
 
