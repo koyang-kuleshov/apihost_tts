f800f7bcf0f8e9fee40938fe7b9aeaa053f84d19
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-04-29 20:31:38 +0300

    Поправил исходный скрипт в соответствии со стандартами

--- apihostTTS.py
+++ apihostTTS.py
@@ -1,40 +1,43 @@
-import requests, shutil, os, time, re
+import requests
+import shutil
+import os
+import time
+import re
 from pygame import mixer
 
 mixer.init()
 
-# Запускаем mp3 файл
+
 def playmp3(filepath):
+    """Запускаем mp3 файл"""
     global stop
-    stop=0
+    stop = 0
     song = mixer.music.load(filepath)
     mixer.music.play()
     while mixer.music.get_busy():
         time.sleep(0.1)
-        if(stop==1):
+        if stop == 1:
             break
     mixer.music.stop()
 
-# Функция говорящая фразу вслух
+
 def speakme(mytext):
-    print('Робот сказал: '+mytext)
-    fm='mp3/'+re.sub('[^а-яА-Я]', '', mytext)
-    fm=fm[:200]
-    fm=fm+'.mp3'
-    if not(os.path.exists(fm)): 
-        mytext=mytext.replace(' ','+')
-        myurl='https://apihost.ru/php/voice.php?&text='+mytext+'&format=mp3&lang=ru-RU&speed=1.0&emotion=neutral&speaker=oksana&robot=1&download'
+    """Функция говорящая фразу вслух"""
+    fm = os.path.join('mp3/', re.sub('[^а-яА-Я]', '', mytext))
+    fm = fm[:200]+'.mp3'
+    if not(os.path.exists(fm)):
+        mytext = mytext.replace(' ', '+')
+        myurl = 'https://apihost.ru/php/voice.php?&text='+mytext+'&format=mp3&'
+        f'lang=ru-RU&speed=1.0&emotion=neutral&speaker=oksana&robot=1&download'
         r = requests.get(myurl, stream=True)
         if r.status_code == 200:
             with open(fm, 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
-            f.close()
+        else:
+            print(f'Сервер вернул код: {r.status_code}')
     playmp3(fm)
+
 
 speakme('Приветствую тебя, Иван!')
 speakme('Сегодня мы научились использовать синтез речи на радость людям!')
-
-
-
-
