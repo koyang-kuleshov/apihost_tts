3fe609f408bd1a6a4b1975d41493ad0d51b73a13
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-05-02 15:25:50 +0300

    Remove unnecessary file saving to disk and add paramert to synthesize function

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,14 @@
 import requests
 import csv
 from os import system, path
+from io import BytesIO
+import wave
 
 from settings import IAM_TOKEN, FOLDER_ID
 
 
 def data():
-    with open('src/texts.csv', "r") as read_f:
+    with open('src/texts2.csv', "r") as read_f:
         reader = csv.DictReader(read_f, delimiter=',')
         for line in reader:
             file_num = line["Номер"] if len(line["Номер"]) > 1 \
@@ -16,16 +18,18 @@ def data():
             raw = path.join('src', raw)
             wav = file_num + file_name + '.wav'
             wav = path.join('wav', wav)
-            with open(raw, 'wb') as f:
-                for audio_content in synthesize(line["Текст"]):
+            with BytesIO() as f:
+                for audio_content in synthesize(line["Текст"], 1.0):
                     f.write(audio_content)
-            system(f'sox -r 48000 -b 16 -e signed-integer -c 1 {raw} \
-                   {wav}')
-            system(f'rm {raw}')
+                f.seek(0)
+                pcmdata = f.read()
+            with wave.open(wav, 'wb') as wavfile:
+                wavfile.setparams((1, 2, 48000, 0, 'NONE', 'not compressed'))
+                wavfile.writeframes(pcmdata)
     return None
 
 
-def synthesize(text):
+def synthesize(text, voice='jane', emotion='evil', spd=1.0):
     url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
     headers = {
         'Authorization': 'Bearer ' + IAM_TOKEN,
@@ -35,7 +39,9 @@ def synthesize(text):
         'text': text,
         'lang': 'ru-RU',
         'folderId': FOLDER_ID,
-        'emotion': 'evil',
+        'voice': voice,
+        'emotion': emotion,
+        'speed': spd,
         'format': 'lpcm',
         'sampleRateHertz': 48000,
     }
