7e5c3dc4971c0c529a6e64ab651ee56a6cae78d7
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-05-16 19:52:05 +0300

    Add translation triggers

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,6 +1,7 @@
 import requests
 import httplib2
 import apiclient.discovery
+import json
 
 from oauth2client.service_account import ServiceAccountCredentials
 from os import path
@@ -19,7 +20,7 @@ def get_data_from_google():
          ]
     )
     httpAuth = credentials.authorize(httplib2.Http())
-    service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
 
     spreadsheet_data = service.spreadsheets().values().get(
         spreadsheetId=SHEETS_ID,
@@ -36,25 +37,24 @@ def data_for_voice(t_lang):
     for line in get_data_from_google():
         file_num = line["#"] if len(line["#"]) > 1 \
             else '0' + line["#"]
-        file_name = '-' + line["Trigger"].replace(' ', '-')
-        raw = file_num + file_name + '.raw'
-        raw = path.join('src', raw)
-        wav = file_num + file_name + '.wav'
-        wav = path.join('wav', wav)
+        file_name = file_num + '-' + line["Trigger"].replace(' ', '-')
+        wav = file_name + '.wav'
+        wav = path.join('wav', t_lang, wav)
         if line["#"] and line[t_lang + '_to_voice']:
             with BytesIO() as f:
                 for audio_content in synthesize(line['Voice_' + t_lang],
-                                                spd=1.0):
+                                                t_lang):
                     f.write(audio_content)
                 f.seek(0)
                 pcmdata = f.read()
             with wave.open(wav, 'wb') as wavfile:
                 wavfile.setparams((1, 2, 48000, 0, 'NONE', 'not compressed'))
                 wavfile.writeframes(pcmdata)
+                print(file_name)
     return None
 
 
-def synthesize(text, voice='jane', emotion='evil', spd=1.0):
+def synthesize(text, lang):
     url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
     headers = {
         'Authorization': 'Bearer ' + IAM_TOKEN,
@@ -62,11 +62,11 @@ def synthesize(text, voice='jane', emotion='evil', spd=1.0):
 
     data = {
         'text': text,
-        'lang': 'ru-RU',
+        'lang': 'ru-RU' if lang == 'ru' else 'en-US',
         'folderId': FOLDER_ID,
-        'voice': voice,
-        'emotion': emotion,
-        'speed': spd,
+        'voice': 'jane' if lang == 'ru' else 'alyss',
+        'emotion': 'evil',
+        'speed': 1.0,
         'format': 'lpcm',
         'sampleRateHertz': 48000,
     }
@@ -80,6 +80,51 @@ def synthesize(text, voice='jane', emotion='evil', spd=1.0):
             yield chunk
 
 
+def translate(what):
+    url = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
+    headers = {
+        'Authorization': 'Bearer ' + IAM_TOKEN,
+        'Content-Type': 'application/json',
+    }
+
+    translated = dict()
+    task = what + '_translate'
+    texts = list()
+    sheet = get_data_from_google()
+    for row in sheet:
+        try:
+            if row[task]:
+                texts.append(row["Number"] + '.' + row["New Trigger"])
+        except KeyError:
+            continue
+
+    data = json.dumps({
+        "sourceLanguageCode": "ru",
+        "targetLanguageCode": "en",
+        "texts": texts,
+        "folderId": FOLDER_ID,
+    })
+
+    with requests.post(url, headers=headers, data=data) as resp:
+        if resp.status_code != 200:
+            raise RuntimeError('Invalid response received: code: %d, message:'
+                               ' %s' % (resp.status_code, resp.text))
+        eggs = json.loads(resp.text)
+        for spam in eggs['translations']:
+            spam = spam['text']
+            spam_num = spam[:spam.find('.')]
+            translated[spam_num] = spam[len(spam_num) + 1:]
+
+    for row in sheet:
+        try:
+            if row[task]:
+                row["Default Trigger"] = translated[row["Number"]]
+        except Exception:
+            continue
+
+    return None
+
+
 def main():
     while True:
         print(
@@ -90,15 +135,15 @@ def main():
               'q. Выход'
         )
         action = input('Выполнить: ')
-        if action == 1:
+        if action == '1':
             data_for_voice('ru')
-        elif action == 2:
-            pass
-        elif action == 3:
-            break
-        elif action == 4:
+        elif action == '2':
+            translate('trigger')
+        elif action == '3':
+            translate('text')
+        elif action == '4':
             data_for_voice('en')
-        elif action == 'q' or 'Q':
+        elif action == ('q' or 'Q'):
             break
         else:
             print('Введён неверный номер')
